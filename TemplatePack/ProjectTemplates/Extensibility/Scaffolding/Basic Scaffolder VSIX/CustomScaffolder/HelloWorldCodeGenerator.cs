0e6d1480daea588a1a91110244e9350f03cee406
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2013-11-17 17:30:36 -0800

    initial commit for scaffolding

---
+++ TemplatePack/ProjectTemplates/Extensibility/Scaffolding/Basic Scaffolder VSIX/CustomScaffolder/HelloWorldCodeGenerator.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Scaffolding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace $safeprojectname$ {
+    public class $safeprojectname$CodeGenerator : CodeGenerator {
+        public $safeprojectname$CodeGenerator(CodeGenerationContext context, CodeGeneratorInformation information)
+            : base(context, information) {
+        }
+        public override void GenerateCode() {
+            AddFile(Context.ActiveProject, "foo.txt", @"C:\Data\Files\template.txt", false);
+        }
+
+        public override bool ShowUIAndValidate() {
+            MessageBox.Show("Scaffolder being invoked");
+            return true;
+        }
+    }
+}
---
+++ TemplatePack/ProjectTemplates/Extensibility/Scaffolding/Basic Scaffolder VSIX/CustomScaffolder/HelloWorldCodeGeneratorFactory.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNet.Scaffolding;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace $safeprojectname$ {
+    [Export(typeof(CodeGeneratorFactory))]
+    public class $safeprojectname$CodeGeneratorFactory : CodeGeneratorFactory {
+        public $safeprojectname$CodeGeneratorFactory()
+            :base(CreateCodeGeneratorInformation()){
+    }
+        public override ICodeGenerator CreateInstance(CodeGenerationContext context) {
+            return new $safeprojectname$CodeGenerator(context,CreateCodeGeneratorInformation());
+        }
+
+        private static CodeGeneratorInformation CreateCodeGeneratorInformation()
+        {
+            return new CodeGeneratorInformation(
+                displayName: "$safeprojectname$ Scaffolder",
+                description: "$safeprojectname$Scaffolder Scaffolder Description",
+                author: "Your Name Here",
+                version: new Version(1, 0, 0, 0),
+                id: typeof($safeprojectname$CodeGenerator).Name,
+                gestures: new[] { (string)null },
+                icon: null,
+                categories: new[] { "$safeprojectname$" }
+                );
+        }
+    }
+}
