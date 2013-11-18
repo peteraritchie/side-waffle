3e9b596ae55e0486637cc5def9d01cf2cee7ab15
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2013-11-18 00:43:54 -0800

    updating scaffolding samples

--- TemplatePack/ProjectTemplates/Extensibility/Scaffolding/Basic Scaffolder VSIX/CustomScaffolder/HelloWorldCodeGenerator.cs
+++ TemplatePack/ProjectTemplates/Extensibility/Scaffolding/Basic Scaffolder VSIX/CustomScaffolder/HelloWorldCodeGenerator.cs
@@ -1,18 +1,22 @@
 ﻿using Microsoft.AspNet.Scaffolding;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
 namespace $safeprojectname$ {
     public class $safeprojectname$CodeGenerator : CodeGenerator {
         public $safeprojectname$CodeGenerator(CodeGenerationContext context, CodeGeneratorInformation information)
             : base(context, information) {
         }
         public override void GenerateCode() {
-            AddFile(Context.ActiveProject, "foo.txt", @"C:\Data\Files\template.txt", false);
+            ICodeGeneratorFilesLocator filesLocator = ServiceProvider.GetService(typeof(ICodeGeneratorFilesLocator)) as ICodeGeneratorFilesLocator;
+            string filePath = filesLocator.GetStaticFilePath("template.txt", TemplateFolders);
+
+            AddFile(Context.ActiveProject, "foo.txt", filePath, false);
         }
 
         public override bool ShowUIAndValidate() {
---
+++ TemplatePack/ProjectTemplates/Extensibility/Scaffolding/Basic Scaffolder VSIX/CustomScaffolder/UI/InputForm.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace $safeprojectname$.UI {
+    /// <summary>
+    /// Interaction logic for InputForm.xaml
+    /// </summary>
+    public partial class InputForm : Window {
+        public InputForm() {
+            InitializeComponent();
+        }
+
+        public string GetTextValue() {
+            return this.TextFieldValue != null ? this.TextFieldValue.Text : null;
+        }
+
+        private void OkClicked(object sender, RoutedEventArgs e) {
+            this.DialogResult = true;
+        }
+
+        private void CancelClicked(object sender, RoutedEventArgs e) {
+            this.DialogResult = false;
+        }
+    }
+}
