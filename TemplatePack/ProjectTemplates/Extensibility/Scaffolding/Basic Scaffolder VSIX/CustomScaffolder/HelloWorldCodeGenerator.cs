a6a4f3b3c27ccbb910e3bab801a03f46bfde2ad9
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2013-11-18 01:09:45 -0800

    updating scaffolding templates

--- TemplatePack/ProjectTemplates/Extensibility/Scaffolding/Basic Scaffolder VSIX/CustomScaffolder/HelloWorldCodeGenerator.cs
+++ TemplatePack/ProjectTemplates/Extensibility/Scaffolding/Basic Scaffolder VSIX/CustomScaffolder/HelloWorldCodeGenerator.cs
@@ -7,8 +7,12 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using $safeprojectname$.UI;
+
 namespace $safeprojectname$ {
     public class $safeprojectname$CodeGenerator : CodeGenerator {
+        protected string Name { get; set; }
+        
         public $safeprojectname$CodeGenerator(CodeGenerationContext context, CodeGeneratorInformation information)
             : base(context, information) {
         }
@@ -17,11 +21,31 @@ namespace $safeprojectname$ {
             string filePath = filesLocator.GetStaticFilePath("template.txt", TemplateFolders);
 
             AddFile(Context.ActiveProject, "foo.txt", filePath, false);
+
+            AddFileFromTemplate(
+                project: Context.ActiveProject,
+                outputPath: "hello",
+                templateName: "hello",
+                templateParameters: new Dictionary<string, object>() 
+                {
+                    {"Name", Name},
+                    {"Namespace", Context.ActiveProject.GetDefaultNamespace()},
+                },
+                skipIfExists: true);
         }
 
         public override bool ShowUIAndValidate() {
             MessageBox.Show("Scaffolder being invoked");
-            return true;
+            InputForm inputForm = new InputForm();
+
+            if (inputForm.ShowDialog()==true) {
+                this.Name = inputForm.GetTextValue();
+
+                return true;
+            }
+            else {
+                return false;
+            }
         }
     }
 }
