8394162d0e513fe415c2f96b1cbe8384ba9b3bcd
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2013-11-17 18:00:37 -0800

    adding custom t4 files

---
+++ TemplatePack/ItemTemplates/Extensibility/Scaffolding/Custom T4 Files/update.ps1
@@ -0,0 +1,11 @@
+﻿$str = ""
+Get-ChildItem *.t4 -Recurse | 
+    ForEach-Object { 
+        $relPath = ((Resolve-Path $_.FullName -Relative).Substring(2))
+
+
+        $str += ("<ProjectItem TargetFileName=""CodeTemplates\$relPath"" ReplaceParameters=""true"">$relPath</ProjectItem>
+" -f $relPath)
+    }
+
+Set-Content C:\temp\scaffoldingtemp.txt -Value $str
